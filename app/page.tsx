16acbf5bec613be452dd3dc44816a1b712ed7ba7
klimetsAN <137374853+klimetsAN@users.noreply.github.com>
2026-05-01 20:43:37 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Color from "./components/overlay/Color";
 import NavBar from "./sections/NavBar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
-import Work from "./sections/Work";
+// import Work from "./sections/Work";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
 import Tools from "./sections/Tools.tsx";
